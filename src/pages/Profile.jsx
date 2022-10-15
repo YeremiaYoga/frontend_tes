c27aad6646a7745d719f6e17d142e73de6fa4c33
Yeremiayoga <yogayeremia@gmail.com>
2022-10-15 22:30:43 +0700

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -31,17 +31,18 @@ const Profile = () => {
     <div>
       <Navbar />
       <div className="mt-20 flex justify-center">
-        <div className="border-4 mx-40 p-10 w-60 space-y-5 border-sky-400">
-          <div className="">
-            <label>Name:</label>
+        <div className="border-4 mx-40 p-10 w-96 space-y-5 border-sky-400">
+          <h1 className="text-center text-2xl font-bold">Profile</h1>
+          <div className=" font-xl font-medium">
+            <label className="font-semibold">Name:</label>
             <p>{name}</p>
           </div>
           <div>
-            <label>Gender:</label>
+            <label className="font-semibold">Gender:</label>
             <p>{gender}</p>
           </div>
           <div>
-            <label>Email:</label>
+            <label className="font-semibold">Email:</label>
             <p>{email}</p>
           </div>
         </div>
